bb3dfcc20290a19756ec9b26211b3f386aa084cf
andrew-wilkes <github@urgentclick.com>
2021-11-11 15:36:03 +0900

    Dynamically update changes to font and text colors

--- addons/color_picker_text_button/color_picker_text_button.gd
+++ addons/color_picker_text_button/color_picker_text_button.gd
@@ -7,10 +7,10 @@ signal color_changed(color)
 export var text_value = "Click Me" setget set_label_text
 export(StyleBoxFlat) var style setget set_styles
 export(float, 0.1, 0.8) var mouseover_darkening_factor = 0.1
-export(DynamicFont) var custom_font
-export(Color) var color
-export(Color) var light_text_color = Color.white
-export(Color) var dark_text_color = Color.black
+export(DynamicFont) var custom_font setget set_font
+export(Color) var color = Color.white setget set_colors_in_tool
+export(Color) var light_text_color = Color.white setget update_light_text_color
+export(Color) var dark_text_color = Color.black setget update_dark_text_color
 export(float, 0.2, 0.8) var text_color_flip_threshold = 0.5
 export var popup_centered = true
 export var simple_color_picker = true
@@ -34,6 +34,8 @@ func _ready():
 	var _e = connect("pressed", self, "show_panel")
 	_e = color_picker.connect("color_changed", self, "set_colors")
 	_e = panel.connect("popup_hide", self, "panel_closed")
+	_e = connect("item_rect_changed", self, "rect_changed")
+
 
 	if simple_color_picker:
 		# Hide color picker elements
@@ -41,13 +43,12 @@ func _ready():
 			color_picker.get_child(n).hide()
 
 	# Make panel resize to fit contents
-	panel.rect_size.y = 0 # Make panel resize to fit contents
+	panel.rect_size.y = 0
 
 	# Initially style the button
-	if Engine.editor_hint:
+	if Engine.editor_hint and style == null:
 		self.style = load("res://addons/color_picker_text_button/styleboxflat.tres")
 		rect_position = Vector2(20, 10)
-		color = Color.white
 
 	setup_label()
 
@@ -72,9 +73,21 @@ func show_hide_label(show_text):
 	center.visible = enable_text
 
 
+func rect_changed():
+	if center:
+		center.rect_size = rect_size
+
+
 func init_size():
-	rect_size = Vector2(200, 60)
-	center.rect_size = rect_size
+	if Engine.editor_hint:
+		rect_size = Vector2(200, 60)
+	rect_changed()
+
+
+func set_font(font):
+	custom_font = font
+	if Engine.editor_hint and label:
+		label.set("custom_fonts/font", font)
 
 
 func set_styles(s):
@@ -118,8 +131,27 @@ func panel_closed():
 	emit_signal("popup_hide")
 
 
+func set_colors_in_tool(c):
+	color = c
+	if Engine.editor_hint and label:
+		set_colors(c)
+
+
+func update_light_text_color(c):
+	light_text_color = c
+	if Engine.editor_hint and label:
+		set_colors(color)
+
+
+func update_dark_text_color(c):
+	dark_text_color = c
+	if Engine.editor_hint and label:
+		set_colors(color)
+
+
 func set_colors(c):
-	emit_signal("color_changed", c)
+	if not Engine.editor_hint:
+		emit_signal("color_changed", c)
 	self_modulate = c
 	color = c
 	var grey_value = (c.r + c.g + c.b) / 3
